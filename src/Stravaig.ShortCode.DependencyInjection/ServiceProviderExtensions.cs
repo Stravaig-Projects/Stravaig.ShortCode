798dd4e6a2ba23f27fbe37b4f5de6414fa92e933
Colin Mackay <github@colinmackay.scot>
2020-12-31 11:28:21 +0000

    Add dependency injection to wire everything up easily

---
+++ src/Stravaig.ShortCode.DependencyInjection/ServiceProviderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Stravaig.ShortCode.DependencyInjection
+{
+    public static class ServiceProviderExtensions
+    {
+        public static IServiceCollection AddShortCodeGenerator<TGenerator>(this IServiceCollection services, Action<ShortCodeOptions> options)
+            where TGenerator : class, IShortCodeGenerator
+        {
+            ShortCodeOptions scOptions = new ShortCodeOptions();
+            options?.Invoke(scOptions);
+
+            services.AddSingleton(options);
+            services.AddSingleton<IEncoder>(p => new Encoder(scOptions.CharacterSpace));
+            services.AddSingleton<IShortCodeGenerator, TGenerator>();
+            services.AddSingleton<IShortCodeFactory, ShortCodeFactory>();
+            return services;
+        }
+    }
+}
