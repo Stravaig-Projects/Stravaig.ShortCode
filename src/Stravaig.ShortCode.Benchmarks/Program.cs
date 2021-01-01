4584db0f7757c290b55ae206fad8e7171cd024ba
Ruiner85 <paul.m.mackay85@gmail.com>
2021-01-01 17:37:03 +0000

    add benchmark

---
+++ src/Stravaig.ShortCode.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stravaig.ShortCode.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+}
