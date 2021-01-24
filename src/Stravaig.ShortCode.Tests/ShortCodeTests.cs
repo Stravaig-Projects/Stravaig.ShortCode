7f6fd227af31b9b83cff2f3cd1aa1821cb8c59d4
Colin Mackay <github@colinmackay.scot>
2021-01-24 11:41:06 +0000

    #22 Add new static ShortCode class

---
+++ src/Stravaig.ShortCode.Tests/ShortCodeTests.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Stravaig.ShortCode.Tests
+{
+    [TestFixture]
+    public class ShortCodeTests
+    {
+        [SetUp]
+        public void Reset()
+        {
+            var staticType = typeof(ShortCode);
+            var constructor = staticType.TypeInitializer ?? throw new InvalidOperationException($"{nameof(ShortCode)} is expected to have a static constructor.");
+            constructor.Invoke(null, Array.Empty<object>());
+        }
+
+        [Test]
+        public void DefaultSettings_HappyPath()
+        {
+            var sequentialCode1 = ShortCode.GenerateSequentialShortCode();
+            sequentialCode1.Length.ShouldBe(7);
+            sequentialCode1.ShouldBe("aaaaaab");
+            
+            var sequentialCode2 = ShortCode.GenerateSequentialShortCode();
+            sequentialCode2.Length.ShouldBe(7);
+            sequentialCode2.ShouldBe("aaaaaac");
+
+            var randomCode1 = ShortCode.GenerateRandomShortCode();
+            randomCode1.Length.ShouldBe(7);
+
+            var randomCode2 = ShortCode.GenerateRandomShortCode();
+            randomCode2.Length.ShouldBe(7);
+            randomCode1.ShouldNotBe(randomCode2);
+        }
+        
+        [Test]
+        [TestCaseSource(sourceName: nameof(Lengths))]
+        public void SetLength_HappyPath(int length)
+        {
+            ShortCode.SetDefaultLength(length);
+            
+            var sequentialCode = ShortCode.GenerateSequentialShortCode();
+            sequentialCode.Length.ShouldBe(length);
+            
+            var randomCode = ShortCode.GenerateRandomShortCode();
+            randomCode.Length.ShouldBe(length);
+        }
+        
+        [Test]
+        [TestCase(10UL, "aaaaaal")]
+        [TestCase(100UL, "aaaaabN")]
+        public void SetSequentialSeed_HappyPath(ulong seed, string value)
+        {
+            ShortCode.SetSequentialSeed(seed);
+            var sequentialCode = ShortCode.GenerateSequentialShortCode();
+            sequentialCode.ShouldBe(value);
+        }
+
+        [Test]
+        [TestCase("0001111", "01")]
+        [TestCase("AAABBBB", "AB")]
+        [TestCase("AAAAADD", "ABCD")]
+        [TestCase("0000030", "01234")]
+        public void SetCharacterSpace_HappyPath(string value, string characterSpace)
+        {
+            ShortCode.SetSequentialSeed(14UL);
+            ShortCode.SetCharacterSpace(characterSpace);
+            var sequentialCode = ShortCode.GenerateSequentialShortCode();
+            sequentialCode.ShouldBe(value);
+        }
+        
+        private static IEnumerable<int> Lengths()
+        {
+            for (int i = 1; i <= 9; i++)
+                yield return i;
+        }
+    }
+}
