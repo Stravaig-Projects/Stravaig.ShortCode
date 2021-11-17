aedba312dfaa7ac332ad3460ad7ba6eadc5698ae
Colin Mackay <github@colinmackay.scot>
2021-11-17 22:52:53 +0000

    Drop calls to obsolete API when targeting .NET 6.0

--- src/Stravaig.ShortCode/CryptographicallyRandomCodeGenerator.cs
+++ src/Stravaig.ShortCode/CryptographicallyRandomCodeGenerator.cs
@@ -5,15 +5,24 @@ namespace Stravaig.ShortCode
 {
     public class CryptographicallyRandomCodeGenerator : IShortCodeGenerator
     {
+        #if !NET6_0
         private readonly RNGCryptoServiceProvider _rng;
+        #endif
+
         public CryptographicallyRandomCodeGenerator()
         {
+            #if !NET6_0
             _rng = new RNGCryptoServiceProvider();
+            #endif
         }
         public ulong GetNextCode()
         {
             byte[] bytes = new byte[8];
+            #if NET6_0
+            RandomNumberGenerator.Fill(bytes);
+            #else
             _rng.GetBytes(bytes);
+            #endif
             return BitConverter.ToUInt64(bytes, 0);
         }
     }
