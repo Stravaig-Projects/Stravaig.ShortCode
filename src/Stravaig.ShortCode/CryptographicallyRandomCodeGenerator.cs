4148088dfccc0d012e38c2b7735769aee7505ca9
Colin Mackay <github@colinmackay.scot>
2023-11-16 11:19:26 +0000

    Drop code specifically for pre-.NET 6.0

--- src/Stravaig.ShortCode/CryptographicallyRandomCodeGenerator.cs
+++ src/Stravaig.ShortCode/CryptographicallyRandomCodeGenerator.cs
@@ -3,7 +3,6 @@ using System.Security.Cryptography;
 
 namespace Stravaig.ShortCode
 {
-#if NET6_0_OR_GREATER
     public class CryptographicallyRandomCodeGenerator : IShortCodeGenerator
     {
         public ulong GetNextCode()
@@ -13,23 +12,4 @@ namespace Stravaig.ShortCode
             return BitConverter.ToUInt64(bytes, 0);
         }
     }
-
-#else // Up to .NET 5.0
-
-    public class CryptographicallyRandomCodeGenerator : IShortCodeGenerator
-    {
-        private readonly RNGCryptoServiceProvider _rng;
-
-        public CryptographicallyRandomCodeGenerator()
-        {
-            _rng = new RNGCryptoServiceProvider();
-        }
-        public ulong GetNextCode()
-        {
-            byte[] bytes = new byte[8];
-            _rng.GetBytes(bytes);
-            return BitConverter.ToUInt64(bytes, 0);
-        }
-    }
-#endif
 }
